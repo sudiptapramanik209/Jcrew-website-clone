83a271c89e7a113fcf2ec8d07e778882a5490bd6
Sourav <souravsahu70047@gmail.com>
2022-09-29 19:29:07 +0530

    Added Files

---
+++ navbar/navbar.js
@@ -0,0 +1,12 @@
+let first=document.getElementById("first");
+let main=document.getElementById("main");
+// main.style.display="none";
+// first.addEventListener("mouseover",(event)=>{
+//     event.preventDefault();
+//     main.style.display="flex";
+// })
+// main.style.display="none";
+// // let navbar=doc;
+// body.addEventListener("mouseover",()=>{
+//     main.style.display="none";
+// })
