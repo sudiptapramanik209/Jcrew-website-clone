91bad01912ec352aaff32acc2758ac3dd0bc3c54
Sudipta Pramanik <sudiptapramanik209@gmail.com>
2022-09-29 19:22:31 +0530

    fill add

--- sign-in/sign_in.js
+++ sign-in/sign_in.js
@@ -9,9 +9,46 @@ const show=()=>{
 }
 
 
+//-------------sign_in----------------//
 
+let signupls = JSON.parse(localStorage.getItem("signup")) || [];
 
+let flag = false;
+const add = () =>{
+  let email=document.getElementById("username").value;
+  let password=document.getElementById("password").value;
+console.log(email,password)
+  
+  if (email == "" || password == "") {
+    alert("Fill the form");
+  } else {
+    //let email=document.getElementById("username").value;
+    //let password=document.getElementById("password").value;
+    
+    let obj = {
+      Email: email,
+      Password: password,
+    };
 
-const add=()=>{
-    console.log("hiii")
+    if (signupls.length <= 0) {
+      alert("User Data is not found please Register");
+      window.location.href = "../sign_up/sign_up.html";
+    } else if (signupls.length >= 1) {
+      for(let i=0;i<signupls.length;i++) {
+        if (obj.Email === signupls[i].email && obj.Password === signupls[i].password) {
+          flag = true;
+          alert("Login Successfully Completed");
+          localStorage.setItem("flag", JSON.stringify(flag));
+          return window.location.href = "#";
+        } else {
+          flag=false;
+        }
+      }
+    }
+  }
+  if(flag===false){
+    alert("Invalid Data")
+  }
 }
+
+//console.log(signupls)
