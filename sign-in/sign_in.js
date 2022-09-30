369a0a0d3677b3af127d509326b6b053937658e9
Sudipta Pramanik <sudiptapramanik209@gmail.com>
2022-09-30 20:03:35 +0530

    fill add

--- sign-in/sign_in.js
+++ sign-in/sign_in.js
@@ -1,3 +1,13 @@
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
 const show = () => {
   let x = document.getElementById("password");
   //console.log(x)
